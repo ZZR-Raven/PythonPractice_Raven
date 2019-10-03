f718d3a291657672434d198028ffab6a2dc28fea
zzraven <zzravenwww@gmail.com>
2019-10-03 17:57:53 +0800

    20191003_2

--- PythonBased/PythonBased_3.py
+++ PythonBased/PythonBased_3.py
@@ -47,4 +47,61 @@ for k,v in LC_dict.items():
 print([k + '=' + v for k,v in LC_dict.items()])# + 连接起来成字符串
 
 #生成器 generator
+#把列表生成器的[]变()
+#保存的是算法，用next()计算出下一个元素的值
+generator_1 = (x*x for x in range(1,11))
+print(next(generator_1))
+print(next(generator_1))
+print(next(generator_1))
+print(next(generator_1))
+print(next(generator_1))
+print(next(generator_1))
+print(next(generator_1))
+print(next(generator_1))
+print(next(generator_1))
+print(next(generator_1))
+#超出会溢出
+#print(next(generator_1))
+for n in generator_1:
+    print(n)
+#并不会打印东西，这个generator已经被迭代完了
+for n in (x*x for x in range(1,11)):
+    print(n)
+#写一个斐波那契数列
+#a, b = b, a + b 等价于 tuple(b,a+b) a = tuple[0], b = tuple[1]
+def fib(max):
+    n,a,b = 0,0,1
+    while n < max:
+        print(b)
+        a, b = b, a + b
+        n = n +1
+    return 'done'
+fib (20)
+#把它变成generator
+#只需要把print变成yield
+#函数运行到return或者最后一句就返回
+#generator函数遇到next执行，遇到yield返回，再次执行时从yield处继续执行
+def fib_generator(max):
+    n,a,b = 0,0,1
+    while n < max:
+        yield(b)
+        a, b = b, a + b
+        n = n +1
+    return 'done'
+#next yield
+def odd():
+    print('step 1')
+    yield 1
+    print('step 2')
+    yield (3)
+    print('step 3')
+    yield (5)
+#odd()
+#next(odd())
+#next(odd())
+o = odd()
+next(o)
+next(o)
+next(o)
+#next(o)    会溢出
 
