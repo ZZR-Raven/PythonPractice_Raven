989d19c89c71ef86b02251610af1b9e8213cf62f
zzraven <zzravenwww@gmail.com>
2019-10-06 21:25:46 +0800

    20191006_8 BP4 打印每章标题

--- PythonCrawler/CH5/p_BS4.py
+++ PythonCrawler/CH5/p_BS4.py
@@ -6,7 +6,15 @@ html = requests.get('https://www.kanunu8.com/book2/10943/').content.decode("gb23
 
 soup = BeautifulSoup(html,'lxml')
 
-useful = soup.find('d1')
+useful = soup.find(class_ ='book')
 all_ch = useful.find_all('dd')
 for dd in all_ch:
     print(dd.string)
+#打印出了每章的标题
+#第1章 全民公敌(1)
+#第2章 全民公敌(2)
+# 第3章 全民公敌(3)
+# 第4章 全民公敌(4)
+# 第5章 全民公敌(5)
+# 第6章 全民公敌(6)
+# 第7章 全民公敌(7)
