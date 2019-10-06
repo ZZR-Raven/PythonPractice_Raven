ecd2981de1e15dc4d1d2aa70793117105b3238f1
zzraven <zzravenwww@gmail.com>
2019-10-06 20:26:49 +0800

    20191006_5

--- PythonCrawler/CH5/p_lxml.py
+++ PythonCrawler/CH5/p_lxml.py
@@ -1,5 +1,35 @@
-import lxml
+import lxml.html
 
+html1 = '''
+<html>
+    <head lang = "en">
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div id="test">
+        我左青龙
+            <span id="tiger">
+                右白虎
+                <ul>上朱雀
+                    <li>下玄武</li>
+                </ul>>
+                老牛在当中
+            </span>
+            龙头在胸口
+        </div>
+    </body>
+</html>
+'''
+
+selector_1 = lxml.html.fromstring(html1) 
+content_1 = selector_1.xpath('//div[@id="test"]/text()')
+for each in content_1:
+    print(each)
+#        我左青龙
+#
+#
+#            龙头在胸口
 
 
 
