97236368057d530d8c68b7bd9a9c9b01ba6828a9
RavenSF <zzravenwww@gmail.com>
2019-10-09 23:56:13 +0800

    20191009_2

--- PythonCrawler/CH6/redis_p.py
+++ PythonCrawler/CH6/redis_p.py
@@ -1,17 +1,8 @@
-import redis
+# import redis
 
-#client = redis.Redis()
-#远程
-#client = redis.StrictRedis(host = '',port = 2739,password = 123456)
+# pool = redis.ConnectionPool()
+# r = redis.Redis(connection_pool=pool)
+# r.set('name', 'raven')   #添加
+# print (r.get('name'))   #获取
 
-#将123放到列表ch6的左侧
-#client.lpush('ch6',123)
-#print(client.llen('ch6'))
-
-
-pool = redis.ConnectionPool()
-r = redis.Redis(connection_pool=pool)
-r.set('name', 'zhangsan')   #添加
-print (r.get('name'))   #获取
-
-
+#玩redis交互环境去了
