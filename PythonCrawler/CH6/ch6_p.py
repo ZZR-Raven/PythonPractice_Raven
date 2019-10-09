26c0680d834819ea4ee866144a12e96a8267d4db
zzraven <zzravenwww@gmail.com>
2019-10-09 17:36:26 +0800

    20191009_0

---
+++ PythonCrawler/CH6/ch6_p.py
@@ -0,0 +1,25 @@
+import scrapy
+import lxml
+import requests
+import redis
+import pymongo
+from lxml import etree
+# from scrapy import Selector
+from scrapy.selector import Selector
+
+with open('title_post.txt','r',encoding='utf-8') as txt1:
+    title_code = txt1.read()
+
+selector = etree.HTML(title_code)
+url_list = selector.xpath('/html/body/div[2]/div[2]/div/dl/dd[1]/a')
+#for url in url_list:
+#   client.lpush('url_queue',url)
+
+content_list = []
+#while client.llen('url_queue') = 0
+#   url = loop('url_queue').decode()
+#   source = requests.get(url).content
+#   selector = html.fromstring(source)
+#   ch_name = selector.xpath('xpath')[0]
+#   content_list.append('title':ch_name,'content':'\n'.join(content))
+#handler.insert(content_list)
