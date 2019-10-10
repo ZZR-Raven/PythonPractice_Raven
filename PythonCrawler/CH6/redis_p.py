fbe2206b82193948c211f6a6c4b271a691e20062
RavenSF <zzravenwww@gmail.com>
2019-10-10 15:11:02 +0800

    20191010_0

--- PythonCrawler/CH6/redis_p.py
+++ PythonCrawler/CH6/redis_p.py
@@ -1,8 +1,20 @@
-# import redis
+import redis
 
-# pool = redis.ConnectionPool()
-# r = redis.Redis(connection_pool=pool)
-# r.set('name', 'raven')   #添加
-# print (r.get('name'))   #获取
+#也可以
+pool = redis.ConnectionPool() 
+client = redis.Redis(connection_pool=pool)
+#client = redis.StrictRedis()
+client.set('name', 'raven')     #添加
+#print (client.get('name'))      #获取 字节类型 b
 
-#玩redis交互环境去了
+#client.mset = (num1 = 'q',num2 = 'w')
+num_dict = {'num1':1,'str1':'1'}    
+client.mset (num_dict)
+# print(client.mget('num1'))
+# print(client.mget('str1'))
+#返回都是字节类型
+
+client.lpush('ch6',233)
+client.rpush('ch6',9999)
+print(client.llen('ch6'))
+
