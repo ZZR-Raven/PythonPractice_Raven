3ba9f45a7aa53279b4f8020e775231b3857d42d4
RavenSF <zzravenwww@gmail.com>
2019-10-11 23:51:47 +0800

    20191011清理无用代码，补充注释

--- PythonCrawler/CH6/ch6_p/ch6_p.py
+++ PythonCrawler/CH6/ch6_p/ch6_p.py
@@ -1,3 +1,7 @@
+#需要连接resis，检查url_lst是否被清空
+#需要清空 龙族前传.txt里内容
+
+
 import scrapy
 import lxml
 import requests
@@ -6,29 +10,30 @@ import pymongo
 from lxml import etree
 from scrapy.selector import Selector
 
+#   redis连接
 pool = redis.ConnectionPool() 
 client = redis.Redis(connection_pool=pool)
 
+#   爬取小说每章节url
 ori_code = requests.get('https://www.kanunu8.com/book3/7750/').content.decode(encoding = 'GB2312')
-with open('title_get.html','w',encoding='utf-8') as txt1:
-    txt1.write(ori_code)
 selector = etree.HTML(ori_code)
 url_list = selector.xpath('//tbody/tr/td/a/@href')
-url_list.pop(0)
+url_list.pop(0)     #去掉第一个无用网址
 
+#   将url补充成html路径并放入redis的list中
 for url in url_list:
     real_url = 'https://www.kanunu8.com/book3/7750/'+url
     client.lpush('url_list',real_url)    
-    # print(real_url) 
 
 count = 1
+#   爬取小说正文
 while client.llen('url_list') != 0 :
     # 从redis list里面获取并删除一个网址
     url_b = client.rpop('url_list')
     url_s = str(url_b,encoding='utf-8')
     if count <= 4:
         ch_code = requests.get(url_s).content.decode(encoding = 'GB2312')
-    else:       #这个沙雕网站前7章跟后面不是同一种编码格式
+    else:       #这个沙雕网站前4章跟后面不是同一种编码格式
         ch_code = requests.get(url_s).content.decode(encoding = 'gbk')
     selector = etree.HTML(ch_code)
     #要从源码看，chrome直接复制xpath可能有多余的误导标签
@@ -41,14 +46,3 @@ while client.llen('url_list') != 0 :
 
 
 
-# while client.scard('url_set') == 0:
-    # 从set里面取一个网址
-    # url = client.spop('url_set')
-    # ch_code = requests.get(url).content.decode(encoding = 'GB2312')
-    # selector = etree.HTML(ch_code)
-    # ch_pre = selector.xpath('//tbody/tr/td/text')
-    # print(ch_pre)
-    # selector = html.fromstring(source)
-    # ch_name = selector.xpath('xpath')[0]
-    # content_list.append('title':ch_name,'content':'\n'.join(content))
-# handler.insert(content_list)
