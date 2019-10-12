cd36e414f9660c5f83ed729e7ad56abfc304fd54
RavenSF <zzravenwww@gmail.com>
2019-10-12 20:23:00 +0800

    20191012完善代码，增加注释

--- PythonCrawler/CH6/ch6_p/ch6_p.py
+++ PythonCrawler/CH6/ch6_p/ch6_p.py
@@ -33,23 +33,24 @@ with open('龙族前传.txt','w',encoding='utf-8') as temp:  #
 count = 1
 #   爬取小说正文
 while client.llen('url_list') != 0 :
-    # 从redis list里面获取并删除一个网址
+    # 从redis list里面获取并删除一个网址(byte),转化成str
     url_b = client.rpop('url_list')
     url_s = str(url_b,encoding='utf-8')
+
+    # get回来的byte自动查找解码格式chardet.detect
     ch_byte = requests.get(url_s).content
     charset_dict = chardet.detect(ch_byte)
     charset = charset_dict['encoding']
     print('charset = ',charset)
+
+    # decode 记得ignore
     ch_code = ch_byte.decode(encoding = charset,errors = 'ignore')
-    # if count <= 4:
-    #     ch_code = requests.get(url_s).content.decode(encoding = 'GB2312')
-    # else:       #这个沙雕网站前4章跟后面不是同一种编码格式
-    #     ch_code = requests.get(url_s).content.decode(encoding = 'gbk')
     selector = etree.HTML(ch_code)
-    #要从源码看，chrome直接复制xpath可能有多余的误导标签
+
+    # 要从源码看，chrome直接复制xpath可能有多余的误导标签
     ch_pre = selector.xpath('/html/body/div[@align="center"]/table/tr/td/p/text()')
-    #print(ch_pre)
-    with open('龙族前传.txt','a',encoding='utf-8') as txt2:  #
+    
+    with open('龙族前传.txt','a',encoding='utf-8') as txt2:  
         txt2.writelines(ch_pre)
         print('ch%sdone'%count)
         count = count + 1
