ff7cecb05cf7db68a8cf3974f9a7e293253a3472
RavenSF <zzravenwww@gmail.com>
2019-10-13 16:31:18 +0800

    20191013找到secret1跟secret2

--- PythonCrawler/CH7/times_ajax.py
+++ PythonCrawler/CH7/times_ajax.py
@@ -17,3 +17,19 @@ print('不写secret1跟secret2，返回:{json.loads(return_json_2.content.decode
 # 不写secret1跟secret2，返回:{json.loads(return_json_2.content.decode())}
 
 
+#   secret_2可以从源代码找到
+# <html>
+#     <head>
+#         <title>exercise ajax load</title>
+#         <script> var secret_2 = 'kingname';</script>
+#     </head>
+#     <body>
+#         <div class="content"></div>
+#     </body>
+#     <script src="static/js/jquery-3.2.1.min.js"></script>
+#     <script src="static/js/loaddata_3.js"></script>
+# </html>
+
+# 其他请求ajax_3_backend 
+# 可以找到secret1，名字是code
+# {"code": "kingname is genius.", "success": true}
