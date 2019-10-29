f5eb5c0bcf309e44d55af26f6a76056e7a911645
RavenSF <zzravenwww@gmail.com>
2019-10-29 13:34:16 +0800

    20191029更新数据分析所需库

--- PythonCrawler/CH8/captcha/captcha_auto_t.py
+++ PythonCrawler/CH8/captcha/captcha_auto_t.py
@@ -2,6 +2,8 @@ import pytesseract
 from PIL import Image
 
 image = Image.open('captcha.png')
+# image.load()        #加载一下图片
+# image.show() 
 code = pytesseract.image_to_string(image)
 print(code)
 
