8b01071e5812a813c37e3c2942ff79fd2783979f
RavenSF <zzravenwww@gmail.com>
2019-11-15 20:16:01 +0800

    20191115sms系统只能拿到本班数据

--- PythonCrawler/CH8/sms/sms_driver.py
+++ PythonCrawler/CH8/sms/sms_driver.py
@@ -1,4 +1,4 @@
-
+﻿
 import re
 import json
 import requests
@@ -35,26 +35,26 @@ print(temp_source)
 num_list = []
 num_list = re.findall('evaluationId=(.*?)&amp;classYearId=13',temp_source,re.S)
 print(num_list)
-num_list = ['4461', '2911', '10233', '10245', '10246', '10249', '10254', '10257', '7490', '10283', '10285', '10298', '10300', '10303', '10305', '10307', '10334', '10337', '10343', '10348', '10362', '10369', '949', '10379', '10392', '6552', '10400', '10407', '4841', '1319', '10431', '3422', '10450', '1024', '6577', '3375', '10475', '10478', '10723', '6563', '4850', '10893', '10900']
+# num_list = ['4461', '2911', '10233', '10245', '10246', '10249', '10254', '10257', '7490', '10283', '10285', '10298', '10300', '10303', '10305', '10307', '10334', '10337', '10343', '10348', '10362', '10369', '949', '10379', '10392', '6552', '10400', '10407', '4841', '1319', '10431', '3422', '10450', '1024', '6577', '3375', '10475', '10478', '10723', '6563', '4850', '10893', '10900']
 
-
-
-
-# target_url = 'http://xsgl.7i5q.cas.scut.edu.cn/sms2/student/evaluation/intellectualList.jsp'
-# session = requests.Session()
-# source = session.get(target_url).content.decode(encoding = 'utf-8',errors = 'ignore')
-# print(type(source))
-# # print(type(source_))
-# with open('html_get.txt','w') as h1:
-#     h1.write(str(source))
-
-
-# with open('zhiyu_info.html',encoding='utf-8') as aaa:
-#     # print(scorelist)
-#     hhh = aaa.read()
-#     url_list = re.findall('<td><a href="(.*?)" target=',str(hhh))
-#     for url in url_list:
-#         real_url.append('http://xsgl.7i5q.cas.scut.edu.cn' + url)
-    
 # code = requests.post('http://xsgl.7i5q.cas.scut.edu.cn/sms2/student/module/evaluation/studentIntellectualDetail.jsp?evaluationId=10573&classYearId=13').content.decode(encoding = 'utf-8',errors = 'ignore')
 # print(code)
+
+real_url = []
+with open('zhiyu_info.html',encoding='utf-8') as aaa:
+    # print(scorelist)
+    hhh = aaa.read()
+    url_list = re.findall('<td><a href="(.*?)" target=',str(hhh))
+    for url in url_list:
+        real_url.append('http://xsgl.7i5q.cas.scut.edu.cn' + url)  
+    scorelist = re.findall('<td>(\\d.\\d)</td>',str(hhh))
+    print(scorelist)
+    finallist = []
+    i = 0
+    for score in scorelist:
+        if i%2 == 0:
+            finallist.append(scorelist[i]) 
+        i = i + 1
+    print(finallist)
+    with open('list.xlsx')as x:
+        x.writelines(finallist)
