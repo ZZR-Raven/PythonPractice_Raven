0ec67cf5cb3812cc72a6a5acbcddf6fc21c72c0a
RavenSF <zzravenwww@gmail.com>
2019-11-19 20:43:47 +0800

    20191119改进旧工程，爬取贴吧非链接文本成功

--- practice/cra1/newcode.py
+++ practice/cra1/newcode.py
@@ -1,3 +1,7 @@
+'''
+由于每层楼都兼顾部分人名变化成百度百科链接
+考虑将链接与其他内容分开爬取再合并
+'''
 import scrapy
 import lxml
 import requests
@@ -11,12 +15,18 @@ from Raven import tqdmRaven
 
 
 url = 'https://tieba.baidu.com/p/4975393487?pn=1'
-xp = '//div[@class="wrap2"]'
+xp_eg = '//*[@id="j_p_postlist"]/div[1]/div[2]/div[1]/cc/div[2]/text()'
+xp_1 =  '//*[@id="j_p_postlist"]/div['
+xp_2 =  ']/div[2]/div[1]/cc/div[2]/text()'
+comments = []
+ori_code = requests.get(url,re.S).content.decode(encoding = 'UTF-8',errors = 'ignore')
 
-# ori_code = requests.get(url).content.decode(encoding = 'UTF-8',errors = 'ignore')
-# with open('new.html','w') as html :
-#     html.write(ori_code) 
+for xp in range(1,26):      # 左闭右开
+    # print(xp)
+    com_xp_1 = xp_1 + str(xp) + xp_2
+    selector = etree.HTML(ori_code)
+    comments.append(selector.xpath(com_xp_1))
+    print(comments)
 
-with open('new.html','r') as html :
-    ori_code = html.read()
-    print(ori_code)
+
+
